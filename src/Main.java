722172f979a450b12fe7f17f79db7458c1b2b1e0
dhermidaperez@danielcastelao.org <dhermidaperez@danielcastelao.org>
2023-02-23 17:22:47 +0100

    Acabado, comentado y funcional

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
 
             System.out.println("Creando socket servidor");
-
+            //Creamos el socket del servidor
             ServerSocket serverSocket = new ServerSocket();
             // Realizamos el bind
             System.out.println("Realizando el bind");
@@ -27,54 +27,80 @@ public class Main {
             //Recibimos las conexion
             System.out.println("Conexion recibida");
 
-
+            //Instanciamos objeto is de la clase Input, porque vamos a recibir
+            //un dato del Cliente
             InputStream is = newSocket.getInputStream();
-            OutputStream os = newSocket.getOutputStream();
+            // Instanciamos objeto de datasource  para poder recibir datos que no sean bytes
             DataInputStream eis = new DataInputStream(is);
+            //Instanciamos objeto de la clase outputStream para enviar en este
+            // caso las operaciones con los resultados que va a recibir el cliente
+            OutputStream os = newSocket.getOutputStream();
+            // Instanciamos objeto dataSource para devolver datos que no sean byte
             DataOutputStream sos = new DataOutputStream(os);
-            boolean conectado = true;
+            // variabe boolean para el bucle mientras sea true el bucle sigue
+            boolean conexion = true;
 
-
+            //Println para indicar que comenzamos hacer los calculos en el menu
             System.out.println("Haciendo las operaciones");
-
-            while (conectado) {
-
+            // Inicilizamos el bucle con el boolean
+            while (conexion) {
+                // variable mensaje para recibir el dato del cliente
+                int mensaje = eis.readInt();
+                //Recibimos el dato del ciente
+                System.out.println("Mensaje  recibido: "+ mensaje);
+                //variable numeró double, porque vamos a devolver un double al cliente,
+                //ya que las operaciones están hechas con esta variable
                 double numero = eis.readDouble();
-                System.out.println("Numero recibido: "+ numero);
-                switch (numero) {
+                //instanciamos el menu
+                switch (mensaje) {
 
                     case 1 -> {
-
-                        os.write( Methods.campoFutbolCuñado(numero));
+                        //llamamos al la clase methods ya hecho en la clase methods,
+                        // y le damos la variable número que va ser enviada al cliente
+                      sos.writeDouble(Methods.campoFutbolCuñado(numero));
 
 
                     }
 
                     case 2 -> {
+                        //llamamos al la clase methods ya hecho en la clase methods,
+                        // y le damos la variable número que va ser enviada al cliente
+                        sos.writeDouble(Methods.jubilacionCuñada(numero));
 
 
                     }
 
                     case 3 -> {
+                        //llamamos al la clase methods ya hecho en la clase methods,
+                        // y le damos la variable número que va ser enviada al cliente
+                        sos.writeDouble(Methods.librosCuñado(numero));
 
                     }
 
                     case 4 -> {
+                        //llamamos al la clase methods ya hecho en la clase methods,
+                        // y le damos la variable número que va ser enviada al cliente
+                        sos.writeDouble(Methods.gasolinaCuñada(numero));
                     }
 
 
                     case 5 -> {
-                        conectado = false;
+                        //bolean false para acabar el buclle
+                        conexion = false;
                    }
                 }
 
             }
-
+            //Printl de que vamos cerrar el socket
             System.out.println("Cerrando el socket");
+            //Cerramos el socket
             newSocket.close();
+            // Printl avisando de que vamos a cerrar el socket del servidor
             System.out.println("Cerrando el socket servidor");
+            //Cerramos socket Servidor
             serverSocket.close();
-            System.out.println("Terminado");
+            //Acabamos
+            System.out.println("Se acabo");
         } catch (IOException ignored) {
         }
     }
